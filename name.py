7e737d734c500340191fb9fe7df78ff06cafdb78
Daniel Armas Ramírez <daniel62armas@gmail.com>
2021-09-23 20:33:39 -0500

    First class

---
+++ name.py
@@ -0,0 +1,13 @@
+name = input('Dame tu nombre ')
+#cout cin ->c++
+print(f'Bienvenido {name}' )
+
+last_name = 'Armas'
+city = 'Actopan'
+age = 18
+skills = ['python', 'javascript']
+
+message = 'Hello ' + name + ' ' + last_name + ' your age is ' + str(age)
+message_2 = f'Hello {name} {last_name} your age is {skills}' #F-string
+print(message)
+print(message_2)
---
+++ script_1.py
@@ -0,0 +1,20 @@
+num_1, num_2 = 3, 6 #Tipo entero
+
+num_3 = 7.898989 #Tipo flotante
+
+num_4 = 4j #Tipo complejo
+
+mensaje = "Hola Mundo"
+mensaje_2  = 'Hola Mundo' #String cadena
+
+
+print(type(num_1))
+print(type(num_3))
+print(type(num_4))
+
+print(type(mensaje))
+print(type(mensaje_2))
+
+arr = [3, 5, 8, 'Hola', 89.9, 4j, [2, 4]]
+print(type(arr))
+print(arr)
